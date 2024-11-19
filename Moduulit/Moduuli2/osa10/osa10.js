708815e5946140fdaba6a9dc5e72ccb8c284a8a2
roflvapor <vapordnala@gmail.com>
2024-11-19 13:44:53 +0200

    should be done

--- Moduulit/Moduuli2/osa10/osa10.js
+++ Moduulit/Moduuli2/osa10/osa10.js
@@ -1,12 +1,27 @@
    'use strict';
 let participantsamount = parseInt(prompt("How many participants: "))
-   const l = []
+   let l = []
    for(let i = 0; i < participantsamount; i++) {
-     let x = prompt("Name of participant: ");
-     l.name = x
-     l.votes = 0
+     let x = prompt("Name of participant "+(i+1)+": ");
+    l.push({name:x,votes:0})
    }
-   l.sort((a, b) => {
-   console.log(a, b);
-   return b - a;
-});
+  console.log(l)
+   const voters = parseInt(prompt("how many voters:"));
+for (let i = 0; i < voters; i++){
+    const who = prompt("Who are you tossing your vote for");
+    const trump = l.find(trump => trump.name === who);
+    if (trump) {
+        trump.votes += 1;
+    }else {
+        console.log("candidate doesnt exist");
+    }
+}
+
+l.sort((a, b) => b.votes - a.votes);
+l.forEach(trump => {
+        console.log(trump.name+": "+ trump.votes +" votes");
+    });
+
+const winner = l[0];
+
+console.log("Election winner is "+winner.name);
--- Moduulit/Moduuli2/osa2/osa2.js
+++ Moduulit/Moduuli2/osa2/osa2.js
@@ -1,14 +1,14 @@
    'use strict';
-   let x = parseInt(prompt("Give amount of doggos"));
+   let x = parseInt(prompt("Give amount of participants"));
   let names = []
   for (let i = 0; i < x; i++) {
     let i2 = i + 1
-   let x1 = prompt("Give name of dog " + i2  );
+   let x1 = prompt("Give name of participant " + i2  );
   names.push(x1);
   }
 
-   let sortedNames = names.sort();
-       let list = document.getElementById("target");
+  let list = document.getElementById("target");
+  names.sort();
    for (let i = 0; i < names.length; ++i) {
        let li = document.createElement('li');
        li.innerText = names[i];
